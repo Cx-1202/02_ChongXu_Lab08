a426ef39e5f4703f1d11a5ed94fefe6e6450d88b
Cx-1202 <chongxu2468@gmail.com>
2020-11-05 17:04:17 +0800

    Initial commit

---
+++ Assets/Script/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    int spacebar = 0;
+    public GameObject scoretext;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.Space))
+        {
+            scoretext.GetComponent<Text>().text = "SpaceBar : " + spacebar;
+            spacebar++;
+        }
+    }
+}    
